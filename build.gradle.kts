7e009acb44f5ae1fd76f336bc7b69f5b187cf22d
jaehafe <wogkdkrm112@gmail.com>
2024-06-13 20:46:11 +0900

    feat: 의존성 추가(kotest, redis, ktlint, querydsl)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     id("org.springframework.boot") version "3.3.0"
     id("io.spring.dependency-management") version "1.1.5"
+    id("org.jlleitschuh.gradle.ktlint") version "11.4.0"
     kotlin("plugin.jpa") version "1.9.24"
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
+    kotlin("kapt") version "1.8.22"
 }
 
 group = "com.kt"
@@ -24,11 +26,20 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    // swagger 의존성
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    // kotest 의존성
+    testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.6.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
 }
 
 kotlin {
--- src/test/kotlin/com/kt/kotlinboard/KotlinBoardApplicationTests.kt
+++ src/test/kotlin/com/kt/kotlinboard/KotlinBoardApplicationTests.kt
@@ -8,6 +8,6 @@ class KotlinBoardApplicationTests {
 
     @Test
     fun contextLoads() {
+        SampleTest()
     }
-
 }
