871bc5109d420f1ccfefce552ace9d2c1568714c
Jaeha Lee <wogkdkrm112@gmail.com>
2024-06-17 00:28:49 +0900

    feat: like create api

--- src/main/kotlin/com/kt/kotlinboard/controller/LikeController.kt
+++ src/main/kotlin/com/kt/kotlinboard/controller/LikeController.kt
@@ -1,19 +1,21 @@
 package com.kt.kotlinboard.controller
 
+import com.kt.kotlinboard.service.LikeService
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class LikeController {
+class LikeController(
+    private val likeService: LikeService,
+) {
 
     @PostMapping("/posts/{postId}/likes")
     fun createLike(
         @PathVariable postId: Long,
         @RequestParam createdBy: String,
     ): Long {
-        println("postId: $postId, createdBy: $createdBy")
-        return 1L
+        return likeService.createLike(postId, createdBy)
     }
 }
---
+++ src/main/kotlin/com/kt/kotlinboard/service/LikeService.kt
@@ -0,0 +1,22 @@
+package com.kt.kotlinboard.service
+
+import com.kt.kotlinboard.domain.Like
+import com.kt.kotlinboard.exception.PostNotFoundException
+import com.kt.kotlinboard.repository.LikeRepository
+import com.kt.kotlinboard.repository.PostRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+class LikeService(
+    private val likeRepository: LikeRepository,
+    private val postRepository: PostRepository
+) {
+    @Transactional
+    fun createLike(postId: Long, createdBy: String): Long {
+        val post = postRepository.findByIdOrNull(postId) ?: throw PostNotFoundException()
+        return likeRepository.save(Like(post, createdBy)).id
+    }
+}
