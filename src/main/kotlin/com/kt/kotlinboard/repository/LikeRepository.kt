94bb9f1e79639ae1eb2e9ecca79e089d2b9c8d5b
jaehafe <wogkdkrm112@gmail.com>
2024-06-17 13:26:15 +0900

    feat: like count

--- src/main/kotlin/com/kt/kotlinboard/repository/LikeRepository.kt
+++ src/main/kotlin/com/kt/kotlinboard/repository/LikeRepository.kt
@@ -3,4 +3,7 @@ package com.kt.kotlinboard.repository
 import com.kt.kotlinboard.domain.Like
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface LikeRepository : JpaRepository<Like, Long>
+interface LikeRepository : JpaRepository<Like, Long> {
+
+    fun countByPostId(postId: Long): Long
+}
--- src/test/kotlin/com/kt/kotlinboard/service/PostServiceTest.kt
+++ src/test/kotlin/com/kt/kotlinboard/service/PostServiceTest.kt
@@ -28,6 +28,7 @@ class PostServiceTest(
     private val postRepository: PostRepository,
     private val commentRepository: CommentRepository,
     private val tagRepository: TagRepository,
+    private val likeService: LikeService
 ) : BehaviorSpec({
     beforeSpec {
         postRepository.saveAll(
@@ -272,6 +273,19 @@ class PostServiceTest(
                 postPage.content[2].title shouldBe "title8"
                 postPage.content[3].title shouldBe "title7"
                 postPage.content[4].title shouldBe "title6"
+            }
+        }
+        When("좋아요가 2개 추가됐을 떄") {
+            val postPage = postService.findPageBy(PageRequest.of(0, 5), PostSearchRequestDto(tag = "tag5"))
+            postPage.content.forEach {
+                likeService.createLike(it.id, "harris1")
+                likeService.createLike(it.id, "harris2")
+            }
+            val likedPostPage = postService.findPageBy(PageRequest.of(0, 5), PostSearchRequestDto(tag = "tag5"))
+            then("좋아요 개수가 정상적으로 조회됨을 확인한다.") {
+                likedPostPage.content.forEach {
+                    it.likeCount shouldBe 2
+                }
             }
         }
     }
